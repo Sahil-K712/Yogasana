53e67dfdeaf08578c3b0c74a66aab2245865d969
Sahil-K712 <sahilkadavekar444@gmail.com>
2025-03-02 20:13:26 +0530

    Fixed deployment issues

--- yogasana-app/vite.config.js
+++ yogasana-app/vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  base: '/',
+  server: {
+    port: 3000,
+    host: true
+  },
+  preview: {
+    port: 3000
+  }
 })
-
